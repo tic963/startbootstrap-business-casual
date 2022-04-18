7f939fe443786ecc868845466964de3346eaaead
tien C <tiencnet@hotmail.com>
2022-04-18 02:01:16 -0700

    add button function in index.html
    try press the vist-us button

--- dist/js/myscript.js
+++ dist/js/myscript.js
@@ -1,5 +1,13 @@
-function myFunction(imgs) {
+const  myFunction= (imgs)=> {
     var expandImg = document.getElementById("expandedImg");
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
 }
+const storeClosed = ()=> {
+    if (new Date().getDay() == 0) {
+        document.getElementById("storeinfo").innerHTML=("We are closed today");
+    } else {
+        document.getElementById("storeinfo").innerHTML = ("We open from 7:00 AM to 8:00 PM");
+    }
+}
+
